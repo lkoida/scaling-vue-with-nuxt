2e13fcbed241c7fb8fc4bcd5fa7c3a7635be816f
lkoida <leonidkoida@gmail.com>
2020-09-16 10:18:16 +0300

    added db.json

---
+++ layouts/error.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="title">
+      {{ message }}
+    </div>
+    <p v-if="statusCode === 404">
+      <nuxt-link to="/">
+        return to home page
+      </nuxt-link>
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Error',
+  props: {
+    error: {
+      type: Object,
+      default: null
+    }
+  },
+  computed: {
+    statusCode () {
+      return (this.error && this.error.statusCode) || 500
+    },
+    message () {
+      return this.error.message
+    }
+  },
+  head () {
+    return {
+      title: this.message
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
